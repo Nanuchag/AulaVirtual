27f82e608c01edb6d3abdb1baa4bb07853c58f71
Nanu Guiralde <nanuguiralde@gmail.com>
2024-05-09 18:13:28 -0300

    parametrizadas - templates

--- AulaVirtual/web/views.py
+++ AulaVirtual/web/views.py
@@ -4,4 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hola Mundo Django!!")
+    return render(request, "index.html")
+
+#funciones parametrizadas
+def saludar(request, nombre):
+
+    print(request.method)
+
+    return HttpResponse(f'Bienvenid@ {nombre}')
+
+def alumnos_por_año(request, year):
+        alumnos = ["Jonathan", "Ellie", "Joel", "Dina"] # """"""""Levanta""""""" los usuarios de la BBDD
+        return HttpResponse(f'listado de alumnos: {year} \n {alumnos}')
