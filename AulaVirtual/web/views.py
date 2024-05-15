a622e20bc1fea59dabeaeb2dbd3a5baf549ad7b8
Nanu Guiralde <nanuguiralde@gmail.com>
2024-05-14 21:39:03 -0300

    Agregamos plantilla a la app

--- AulaVirtual/web/views.py
+++ AulaVirtual/web/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+
+    # Accedo a la BBDD a traves de los modelos
+    context = {
+         'nombre': 'Carlos',
+         'fecha_hora': datetime.datetime.now()
+    }
+    return render(request, 'web/index.html', context)
 
 #funciones parametrizadas
 def saludar(request, nombre):
@@ -16,3 +23,18 @@ def saludar(request, nombre):
 def alumnos_por_año(request, year):
         alumnos = ["Jonathan", "Ellie", "Joel", "Dina"] # """"""""Levanta""""""" los usuarios de la BBDD
         return HttpResponse(f'listado de alumnos: {year} \n {alumnos}')
+
+
+def listado_alumnos(request):
+     #estruturas cíclicas con templates
+     context = {
+          'alumnos': [
+               'Carlos Lopez',
+               'Maria Del Cerro',
+               'Gaston Perez'
+          ],
+          'cuota_al_dia': True
+     }
+      
+
+     return render(request, 'web/listado_alumnos.html', context)
